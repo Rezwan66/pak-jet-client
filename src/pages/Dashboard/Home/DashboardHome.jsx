596a70dd764613a970103d8b935ba0875f4dbb87
Rezwan66 <shaikhrezwan66@gmail.com>
2025-11-01 20:05:53 +0100

    edited dashboard homepage

--- src/pages/Dashboard/Home/DashboardHome.jsx
+++ src/pages/Dashboard/Home/DashboardHome.jsx
@@ -1,8 +1,13 @@
+import useAuth from '../../../hooks/useAuth';
+
 const DashboardHome = () => {
+  const { user } = useAuth();
   return (
     <>
       <div>
         <h2> HELLO I Am DashboardHome </h2>
+        <p>Here we will add some basic dashboard options like user info</p>
+        {user && <span>UserName: {user?.displayName || 'Login Now'}</span>}
       </div>
     </>
   );
